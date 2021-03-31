81c76aaeede00595075cc50998787d6059213ad7
Crowdbotics <team@crowdbotics.com>
2021-03-31 09:27:09 +0000

    Update models

--- backend/mr_messenger_25380/wsgi.py
+++ backend/mr_messenger_25380/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mr_messenger_25380.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mr_messenger_25380.settings")
 
 application = get_wsgi_application()
